ae38a09c37a9f96647d1bf13f3ce60774af6ce32
KING <matadeenyadav202@gmail.com>
2026-02-14 20:57:59 +0530

    Create 2.Variables_Datatypes.java

---
+++ 2.Variables_Datatypes.java
@@ -0,0 +1,28 @@
+public class Java_Variables {
+    static void main(String[] args) {
+
+        boolean isPassed = true;
+        int count = 23;
+        byte num = 25;
+
+        int counts = 99;
+        long countslong = counts;
+        System.out.println(counts);
+
+        long dev = 13;
+        int devint = (int)dev;
+
+        System.out.println(dev);
+
+
+        System.out.println(isPassed);  // if you use only print the output of next line will continue in same line,
+        // so use println instead of print.
+        System.out.println(count);
+        System.out.println(num);
+
+
+
+    }
+
+
+}
