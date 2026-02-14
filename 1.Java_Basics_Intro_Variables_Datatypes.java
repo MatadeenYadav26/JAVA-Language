9a07f23292b796052a88072353b261325f009ae4
KING <matadeenyadav202@gmail.com>
2026-02-14 20:29:45 +0530

    Create 1.Java_Basics_Intro_Variables_Datatypes.java

---
+++ 1.Java_Basics_Intro_Variables_Datatypes.java
@@ -0,0 +1,19 @@
+public class Java_Basics { // this is a block
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(98);
+        System.out.println(98+2);
+        System.out.println(98*2);
+        System.out.println(98/2);
+        System.out.println(98%2);
+        System.out.println("98+2");
+
+        // variables:
+        int age = 26;
+        int amanYadav = 21;
+        System.out.println(age);
+        System.out.println(amanYadav);
+        
+    }
+}
