c832310f634d2f5436a217e3236c696bb7239187
KING <matadeenyadav202@gmail.com>
2026-03-09 15:07:36 +0530

    Create 13. Problems on Foreach Loop.java

---
+++ 13. Problems on Foreach Loop.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class problems_on_arrays {
+//// sum of numbers in an Array:
+    static void main(String[] args) {
+        int numbers[] = {-99,-899,999,-10,20,30,40,50};
+////        int sum = 0 ;
+////        for (int number : numbers){
+////            sum += number;
+////        }
+//        System.out.println("Sum is: "+sum);
+//
+        // return the largest number in an array:
+//        int temp = Integer.MIN_VALUE ;
+//        for (int number : numbers){
+//            if(number > temp){
+//                temp = number;
+//            }
+//        }
+//        System.out.println("Maximum number is: "+ temp);
+//
+//        // return the smallest number in an array:
+////        int temp = Integer.MIN_VALUE ; //use integer wrapper class : integerMAXvalue , yaha pe integer ka max value is alreadyy stored and vo use hga for comparision.
+////        for (int number : numbers){
+////            if(number < temp){
+////                temp = number;
+////            }
+//        }
+//        System.out.println("Smallest number is: "+ temp);
+
+
+
+    }
+}
