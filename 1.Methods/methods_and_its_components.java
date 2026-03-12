45b4c7dd96288226d4fd381eeaf09eeb9dee1f75
KING <matadeenyadav202@gmail.com>
2026-03-13 00:09:29 +0530

    Create methods_and_its_components.java

---
+++ 1.Methods/methods_and_its_components.java
@@ -0,0 +1,25 @@
+package Methods;
+
+public class LearnMethods {
+    static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        greet();
+        System.out.println(3);
+        System.out.println(4);
+    }
+
+
+    // Note : access modifier is optional in general.
+
+    static public void greet(){
+        System.out.println(5);
+        System.out.println("Hello World!");
+        System.out.println(6);
+    }
+
+//    public int average(int a , int b){
+//
+//    }
+
+}
