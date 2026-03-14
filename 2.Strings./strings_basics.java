8f3aa9c489822da0c03dc8134a773f9387e519a8
KING <matadeenyadav202@gmail.com>
2026-03-14 11:30:02 +0530

    Create strings_basics.java

---
+++ 2.Strings./strings_basics.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class strings_basic {
+    public static void main(String[] args) {
+        String name = "Aman";
+        String samename = "Aman";
+        String newName = new String("Aman");
+
+        System.out.println(name);
+        System.out.println(samename);
+        System.out.println(newName);
+
+        System.out.println("Aman kumar"); // here also a string is made but the data isn't stored into the variable so , therefore : it runs and vanishes of from system after printing.
+
+    }
+}
